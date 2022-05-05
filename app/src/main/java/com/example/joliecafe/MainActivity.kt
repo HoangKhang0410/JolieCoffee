0a5e8023dc60005fc5b81b7c204be6ff1186c499
maiphuocloi01 <maiphuocloi01@gmail.com>
2022-05-05 18:09:15 +0700

    fix status bar

--- app/src/main/java/com/example/joliecafe/MainActivity.kt
+++ app/src/main/java/com/example/joliecafe/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.joliecafe
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 
@@ -9,10 +14,34 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_JolieCafe)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
